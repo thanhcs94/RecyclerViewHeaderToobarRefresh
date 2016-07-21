f2110ac20940fe4121845312f4b6ba3fccfb4422
ThanhNguyen94 <nguyenvanthanh.cs94@gmail.com>
2016-07-22 00:43:08 +0700

    add  hide/show toolbar when recycleview scroll

--- sample/src/main/java/com/bartoszlipinski/recyclerviewheader2/sample/fragment/SampleGridFragment.java
+++ sample/src/main/java/com/bartoszlipinski/recyclerviewheader2/sample/fragment/SampleGridFragment.java
@@ -15,13 +15,19 @@
  */
 package com.bartoszlipinski.recyclerviewheader2.sample.fragment;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.LinearInterpolator;
 
 import com.bartoszlipinski.recyclerviewheader2.RecyclerViewHeader;
 import com.bartoszlipinski.recyclerviewheader2.sample.R;
@@ -35,7 +41,8 @@ public class SampleGridFragment extends Fragment {
 
     private RecyclerViewHeader recyclerHeader;
     private RecyclerView recycler;
-
+    private Toolbar tToolbar;
+    int TOOLBAR_ELEVATION = 4;
     public static SampleGridFragment newInstance() {
         SampleGridFragment fragment = new SampleGridFragment();
         return fragment;
@@ -50,12 +57,113 @@ public class SampleGridFragment extends Fragment {
 
     private void setupViews(View view) {
         recycler = (RecyclerView) view.findViewById(R.id.recycler);
-        GridLayoutManager layoutManager = new GridLayoutManager(getActivity(), 3);
+        tToolbar = (Toolbar)view.findViewById(R.id.toolbar_top);
+        GridLayoutManager layoutManager = new GridLayoutManager(getActivity(), 2);
         recycler.setLayoutManager(layoutManager);
-        recycler.setAdapter(new ColorItemsAdapter(getActivity(), 21));
+        recycler.setAdapter(new ColorItemsAdapter(getActivity(), 50));
 
         recyclerHeader = (RecyclerViewHeader) view.findViewById(R.id.header);
         recyclerHeader.attachTo(recycler);
+
+        recycler.addOnScrollListener(new RecyclerView.OnScrollListener() {
+
+            // Keeps track of the overall vertical offset in the list
+            int verticalOffset;
+
+            // Determines the scroll UP/DOWN direction
+            boolean scrollingUp;
+
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    if (scrollingUp) {
+                        if (verticalOffset > tToolbar.getHeight()) {
+                            toolbarAnimateHide();
+                        } else {
+                            toolbarAnimateShow(verticalOffset);
+                        }
+                    } else {
+                        if (tToolbar.getTranslationY() < tToolbar.getHeight() * -0.6 && verticalOffset > tToolbar.getHeight()) {
+                            toolbarAnimateHide();
+                        } else {
+                            toolbarAnimateShow(verticalOffset);
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public final void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                verticalOffset += dy;
+                scrollingUp = dy > 0;
+                int toolbarYOffset = (int) (dy - tToolbar.getTranslationY());
+                tToolbar.animate().cancel();
+                if (scrollingUp) {
+                    if (toolbarYOffset < tToolbar.getHeight()) {
+                        if (verticalOffset > tToolbar.getHeight()) {
+                            toolbarSetElevation(TOOLBAR_ELEVATION);
+                        }
+                        tToolbar.setTranslationY(-toolbarYOffset);
+                    } else {
+                        toolbarSetElevation(0);
+                        tToolbar.setTranslationY(-tToolbar.getHeight());
+                    }
+                } else {
+                    if (toolbarYOffset < 0) {
+                        if (verticalOffset <= 0) {
+                            toolbarSetElevation(0);
+                        }
+                        tToolbar.setTranslationY(0);
+                    } else {
+                        if (verticalOffset > tToolbar.getHeight()) {
+                            toolbarSetElevation(TOOLBAR_ELEVATION);
+                        }
+                        tToolbar.setTranslationY(-toolbarYOffset);
+                    }
+                }
+            }
+        });
     }
+
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void toolbarSetElevation(float elevation) {
+        // setElevation() only works on Lollipop
+        // Check if we're running on Android 5.0 or higher
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            // Call some material design APIs here
+            tToolbar.setElevation(elevation);
+        } else {
+            // Implement this feature without material design
+        }
+
+    }
+
+    private void toolbarAnimateShow(final int verticalOffset) {
+        tToolbar.animate()
+                .translationY(0)
+                .setInterpolator(new LinearInterpolator())
+                .setDuration(180)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+                        toolbarSetElevation(verticalOffset == 0 ? 0 : TOOLBAR_ELEVATION);
+                    }
+                });
+    }
+
+    private void toolbarAnimateHide() {
+        tToolbar.animate()
+                .translationY(-tToolbar.getHeight())
+                .setInterpolator(new LinearInterpolator())
+                .setDuration(180)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        toolbarSetElevation(0);
+                    }
+                });
+    }
+
 
 }
